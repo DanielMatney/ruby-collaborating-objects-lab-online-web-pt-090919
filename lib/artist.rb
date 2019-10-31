f6ae55b7a20ab4fdb54b536b3358ad4517bcb245
Daniel Matney <danieljmatney@gmail.com>
2019-10-31 18:42:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,12 +20,23 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
+<<<<<<< HEAD
     @@all.each do |artist|
       if artist.name == name
         return artist
       end
     end
     artist = Artist.new(name)
+=======
+    Artist.all.each do |artist|
+      if artist.name == name
+        return artist
+      else
+        artist = Artist.new(name)
+        return artist
+      end
+    end
+>>>>>>> 06c5f569882701503dc32bd84460aa96211e69d4
   end
   
   def print_songs
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 require 'pry'
 class Song
   attr_accessor :name, :artist
+=======
+class Song
+  attr_accessor :name, :artist, :artist_name
+>>>>>>> 06c5f569882701503dc32bd84460aa96211e69d4
   @@all = []
   
   def initialize(name)
@@ -14,6 +19,7 @@ class Song
   
   def self.new_by_filename(filename)
     array = filename.split(" - ")
+<<<<<<< HEAD
     name = array[0]
     song_name = array[1]
     song = Song.new(song_name)
@@ -24,5 +30,10 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
+=======
+    song_name = array[1]
+    name = array[0]
+    song = Song.new(song_name)
+>>>>>>> 06c5f569882701503dc32bd84460aa96211e69d4
   end
 end
